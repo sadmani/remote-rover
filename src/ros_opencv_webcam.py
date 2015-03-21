a444e1993111f07b253f739193c6cee3ecf8e516
Sabeen Admani <sabeen.admani@gmail.com>
2015-03-20 21:23:15 -0500

    changed the topic name from /robot_vision to /camera/image_raw for future use with robot web tools

--- src/ros_opencv_webcam.py
+++ src/ros_opencv_webcam.py
@@ -1,3 +1,11 @@
+#ros_opencv_webcam: when the host computer is connected to the wifi module on
+#the rover, this script will capture images from the webcam on the rover and
+#stream it to a rostopic using opencv. The purpose of this piece of code is to
+#see the world through the rover's "eyes"
+
+##Note: I found out how to do a lot of this through a few stackexchange.com
+##questions that were asked previously
+
 #!/usr/bin/env python
 import roslib
 import rospy
@@ -9,14 +17,19 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 def wifi_camera():
+	#Create a publisher for the images
 	pub = rospy.Publisher('robot_vision',Image)
+	#Initialize the node
 	rospy.init_node('wifi_camera',anonymous=True)
+	#Open the mjpeg stream using the below line; this address was found when I
+	#went through the Windows GUI that was developed by the company who built
+	#the rover. Originally, it was written to show snapshots, but I edited so
+	#it produces a video stream instead
 	cam = cv2.VideoCapture("http://192.168.1.1:8080/?action=stream&amp;type=.mjpg")
 	print cam.isOpened()
 	bridge = CvBridge()
 	while(True):
 	        ret, frame = cam.read()
-	        #cv2.imshow('frame', frame)
 	        image_message = bridge.cv2_to_imgmsg(frame,encoding="bgr8")
 	        pub.publish(image_message)
 	        if cv2.waitKey(1) & 0xFF == ord('q'):
