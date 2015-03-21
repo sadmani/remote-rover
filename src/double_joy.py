12c06f0059cb58cd96ebbcf1dc2e3d04d0116cee
Sabeen Admani <sabeen.admani@gmail.com>
2015-03-20 21:22:18 -0500

    added comments to double_joy

--- src/double_joy.py
+++ src/double_joy.py
@@ -6,6 +6,13 @@ from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist,Vector3
 from math import sqrt
 
+
+#double_joy: listens to the joy_topic published by joy_node and publishes a
+#twist to control rover movement at a lower frequency; this is because the
+#joy_node is publishing at 100 Hz when we can't even send messages that fast
+#through rosserial
+
+
 output = Joy()
 mvt = Twist()
 prev_mvt = Twist()
@@ -21,7 +28,8 @@ prev_mvt = Twist(prev_lin_vel,Vector3(0,0,prev_wz))
 
 
 def joy_buffer(data):
-	#print(prev_mvt)
+	#Listens to joy node and publishes the same data to a topic called
+	#'buffered_joy' at a lower rate
 	pubBuffJoy = rospy.Publisher('buffered_joy',Joy)
 	buff_data = data
 
@@ -30,6 +38,8 @@ def joy_buffer(data):
 
 
 def send_mvt(data):
+	#Listens to the 'buffered_joy' topic and publishes a twist to control
+	#rover movement using that information; also publishes at a lower rate
 	pubBuffTwist = rospy.Publisher('buffered_twist',Twist)
 	
 	global prev_lin_vel
@@ -40,28 +50,23 @@ def send_mvt(data):
 	lin_vel.y = data.axes[1]
 	lin_vel.z = 0
 
+	#wz is equal to 1 to show that the rover is turning
 	if lin_vel.x != 0 and lin_vel.y != 0:
 		wz = 1
 	else:
 		wz = 0
 
 	mvt = Twist(lin_vel,Vector3(0,0,wz))
-	#print('mvt twist: ', mvt)
 
+	#only send the twist if it isn't equal to the previous twist value
 	if prev_mvt != mvt:
 		twist_to_send = mvt
 		pubBuffTwist.publish(twist_to_send)
 	
-	#prev_mvt = mvt
 	prev_lin_vel.x = lin_vel.x
 	prev_lin_vel.y = lin_vel.y
 	prev_wz = wz
 
-#	prev_mvt = Twist(prev_lin_vel,Vector3(0,0,wz))
-
-#	pubBuffTwist.publish(twist_to_send)
-
-#	pubBuffTwist.publish(mvt)
 	rospy.Rate(20)
 
 
